3754c31399e93bf2c5beaf6b215ad44711137894
alex narayan <alex@alexandarnarayan.com>
2022-12-18 23:21:47 +0100

    done, but this could be a lot better

--- python/solution.py
+++ python/solution.py
@@ -10,25 +10,32 @@ class InputAndTarget:
 
 @dataclass(frozen=True)
 class Result:
-    indexes: set[int]
+    indexes: set[tuple[int, int]]
     found: bool
 
 
-def get_list_of_nums_and_target(file: str)-> (InputAndTarget | None):
+def get_list_of_nums_and_target(file: str)-> InputAndTarget:
     p = Path(DATA_FILE)
     with p.open() as data:
             nums: str = data.readline()
-            list_of_nums: list[str] = nums.strip(' \n').split(' ')
+            list_of_nums: list[str] = nums.strip(' \n').split(' ') # text file has a trailing space before the new line, TODO: fix this?
             list_of_integers: list[int] = [int(x) for x in list_of_nums]
             target: int = int(data.readline().strip('\n'))
             result: InputAndTarget = InputAndTarget(candidates=list_of_integers, target=target)
             return result
 
+def find_indexes_if_any(request: InputAndTarget)->Result:
+    for index, value in enumerate(request.candidates):
+        if (compliment:= request.target - value) in request.candidates:
+            indexes=set([tuple([index, value]), tuple([request.candidates.index(compliment), compliment])])
+            return Result(indexes=indexes, found=True)
+    return Result(indexes=set(), found=False)
 
-    
+
 
 if __name__ == '__main__':
-    input_and_target = get_list_of_nums_and_target(file=DATA_FILE)    
+    input_and_target = get_list_of_nums_and_target(file=DATA_FILE)
     print(input_and_target)
+    print(find_indexes_if_any(input_and_target))
 
 
