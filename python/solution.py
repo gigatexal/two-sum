ce3abec67d87af113c78e8c6d0ef72d94688c898
alex narayan <alex@alexandarnarayan.com>
2022-12-18 22:35:30 +0100

    remove try except for the time being

--- python/solution.py
+++ python/solution.py
@@ -15,25 +15,20 @@ class Result:
 
 
 def get_list_of_nums_and_target(file: str)-> (InputAndTarget | None):
-    with Path(DATA_FILE).open() as data:
-        nums: str = data.readline()
-        list_of_nums: list[str] = nums.strip(' \n').split(' ')
-        list_of_integers: list[int] = [int(x) for x in list_of_nums]
-        target: int = int(data.readline().strip('\n'))
-        result: InputAndTarget = InputAndTarget(candidates=list_of_integers, target=target)
-        return result
-    return None
-
+    p = Path(DATA_FILE)
+    with p.open() as data:
+            nums: str = data.readline()
+            list_of_nums: list[str] = nums.strip(' \n').split(' ')
+            list_of_integers: list[int] = [int(x) for x in list_of_nums]
+            target: int = int(data.readline().strip('\n'))
+            result: InputAndTarget = InputAndTarget(candidates=list_of_integers, target=target)
+            return result
 
 
     
-
-
 
 if __name__ == '__main__':
-    list_of_numbers: list[int] = []
-    target: int = -1
-    print(get_list_of_nums_and_target(file=DATA_FILE))
-    
+    input_and_target = get_list_of_nums_and_target(file=DATA_FILE)    
+    print(input_and_target)
 
 
